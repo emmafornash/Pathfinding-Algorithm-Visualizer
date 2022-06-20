84d86b1680e7d36df21c91af5f89f472739cc099
emmafornash <emmafornash@gmail.com>
2022-06-20 13:52:07 -0400

    set up boilerplate code for the window

--- visualizer.py
+++ visualizer.py
@@ -1,2 +1,23 @@
+from tkinter import messagebox, Tk
+import pygame
+import sys
+
+# constants
+WIN_WIDTH = 500
+WIN_HEIGHT = 500
+
 def main() -> None:
-    print("test")
+    win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+    pygame.display.set_caption("Pathfinding Visualizer")
+    while True:
+        for event in pygame.event.get():
+            # quit event
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        win.fill((0,0,0))
+
+        pygame.display.flip()
+
+main()
